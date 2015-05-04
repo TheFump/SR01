ddef0a99d96d9c6e479bf5f502ca2be23b03bf2c
TheFump <alexandre.guth@etu.utc.fr>
2015-05-04 11:20:40 +0200

    Create arg2.c
    Variable environnement/2

---
+++ arg2.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+extern char ** environ;
+
+int main (void)
+{
+	int	i = 0;
+
+	for (i = 0; environ[i] != NULL; i ++)
+		fprintf(stdout, "%d : %s\n", i, environ[i]);
+	return 0;
+}
+
